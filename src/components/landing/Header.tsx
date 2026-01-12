c531de77c86f9ceecb7809bfec62e181747f5e1c
distribuidoraledlp-spec <distribuidoraledlp@gmail.com>
2026-01-12 19:57:05 -0300

    Update logo image size in Header component

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
             <img 
               src={logo} 
               alt="La Plata LED" 
-              className="h-10 md:h-12 w-auto"
+              className="h-12 md:h-14 lg:h-16 w-auto"
             />
           </a>
           
